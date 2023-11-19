4256f900db63ead50f11e7c973262668c592fd49
Zaid <syedzaidahmed21@gmail.com>
2023-11-20 02:14:47 +0500

    first commit

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ window.addEventListener("scroll", function () {
     navbar.style.background = "rgba(39, 20, 18, 0.5)";
 });
 
+const homeBtn = document.getElementById("homeBtn");
+
+window.addEventListener("scroll", function () {
+  window.scrollY <= 500
+    ? (homeBtn.style.display = "none")
+    : (homeBtn.style.display = "block");
+});
+
+const togglerBtn = document.getElementById("togglerBtn");
+const navUl = document.getElementById("navUl");
+togglerBtn.addEventListener("click", () => {
+  navUl.style.display = "flex";
+  togglerBtn.innerText = "Change";
+});
